bceab5b225c6c6ee73ea287d289b7477832aad8f
BrunoVernay <BrunoVern.a@gmail.com>
2017-01-13 14:44:00 +0100

    init
    Change-Id: I59d9f112023c30811ae7381761ab67d9e97f6eaa

---
+++ src/main/java/Quickstart.java
@@ -0,0 +1,115 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.slides.v1.SlidesScopes;
+import com.google.api.services.slides.v1.model.*;
+import com.google.api.services.slides.v1.Slides;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Quickstart {
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+        "Google Slides API Java Quickstart";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+        System.getProperty("user.home"), ".credentials/slides.googleapis.com-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+        JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/slides.googleapis.com-java-quickstart
+     */
+    private static final List<String> SCOPES =
+        Arrays.asList(SlidesScopes.PRESENTATIONS_READONLY);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+            Quickstart.class.getResourceAsStream("/client_cred.json");
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Slides API client service.
+     * @return an authorized Slides API client service
+     * @throws IOException
+     */
+    public static Slides getSlidesService() throws IOException {
+        Credential credential = authorize();
+        return new Slides.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Build a new authorized API client service.
+        Slides service = getSlidesService();
+
+        // Prints the number of slides and elements in a sample presentation:
+        // https://docs.google.com/presentation/d/1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc/edit
+        String presentationId = "1piFaL4ttKNAv6_lkUgJLLSIRSah7blvd8ghqMFEeNnk";
+        Presentation response = service.presentations().get(presentationId).execute();
+        List<Page> slides = response.getSlides();
+
+        System.out.printf("The presentation contains %s slides:\n", slides.size());
+        for (int i = 0; i < slides.size(); i++) {
+            Page slide = slides.get(i);
+            System.out.printf("- Slide #%s contains %s elements.\n", i + 1,
+                    slide.getPageElements().size());
+        }
+    }
+
+
+}
